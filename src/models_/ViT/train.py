52f1c10614c81f023ab3321aca7cf6a65751a43e
HJacksons <jacksonherberts@gmail.com>
2024-02-08 10:23:17 +0100

    review train and valid

--- src/models_/ViT/train.py
+++ src/models_/ViT/train.py
@@ -32,7 +32,7 @@ loss_fn = nn.CrossEntropyLoss()
 for epoch in range(data_config.EPOCHS):
     # Train the model
     model.train()
-    train_loss, train_accuracy = 0, 0
+    train_loss, train_correct, total_train_samples = 0, 0, 0
     for images, labels in train_loader:
         if torch.is_tensor(images):
             images = [to_pil_image(img) for img in images]
@@ -41,21 +41,26 @@ for epoch in range(data_config.EPOCHS):
         labels = labels.to(data_config.DEVICE)
 
         # Forward pass
-        outputs, loss = model(pixel_values, None)
-        if loss is None:
-            loss = loss_fn(outputs, labels)
-            # Backward pass and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        outputs = model(pixel_values)
+        loss = loss_fn(outputs, labels)
 
-            train_loss += loss.item()
-            predictions = torch.argmax(outputs, 1)
-            train_accuracy += (predictions == labels).sum().item()
+        # Backward pass and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        predictions = torch.argmax(outputs, 1)
+        train_correct += (predictions == labels).sum().item()
+        total_train_samples += labels.size(0)
+
+        # Train accuracy and loss
+        train_loss /= len(train_loader)
+        train_accuracy = train_correct / total_train_samples
 
         # Validation
         model.eval()
-        val_loss, val_accuracy = 0, 0
+        val_loss, val_correct, total_val_samples = 0, 0, 0
         with torch.no_grad():
             for images, labels in vali_loader:
                 if torch.is_tensor(images):
@@ -65,20 +70,20 @@ for epoch in range(data_config.EPOCHS):
                 labels = labels.to(data_config.DEVICE)
 
                 # Forward pass
-                outputs, loss = model(pixel_values, None)
-                if loss is None:
-                    loss = loss_fn(outputs, labels)
+                outputs = model(pixel_values)
+                loss = loss_fn(outputs, labels)
 
-                    # Calculate loss and accuracy
-                    val_loss += loss.item()
-                    predictions = torch.argmax(outputs, 1)
-                    val_accuracy += (predictions == labels).sum().item()
+                # Calculate loss and accuracy
+                val_loss += loss.item()
+                predictions = torch.argmax(outputs, 1)
+                val_correct += (predictions == labels).sum().item()
+                total_val_samples += labels.size(0)
+
+                # Validation accuracy and loss
+                val_loss /= len(vali_loader)
+                val_accuracy = val_correct / total_val_samples
 
         # Log training and validation results
-        train_loss /= len(train_loader)
-        train_accuracy /= len(train_loader)
-        val_loss /= len(vali_loader)
-        val_accuracy /= len(vali_loader)
         logging.info(
             f"Epoch: {epoch} | Train loss: {train_loss:.4f} | Train accuracy: {train_accuracy:.4f} | Val loss: {val_loss:.4f} | Val accuracy: {val_accuracy:.4f}"
         )
