99177fd6d70525a2e29a104aecc13b83d3919c08
HJacksons <jacksonherberts@gmail.com>
2024-02-10 02:02:48 +0100

    transform inout as per model type

--- src/models_/features/extract_features_and_combine.py
+++ src/models_/features/extract_features_and_combine.py
@@ -24,7 +24,7 @@ ViT_model.to(data_config.DEVICE)
 inception_dataset = DatasetPreparer(model_type="inception")
 _, _, inception_test_loader = inception_dataset.prepare_dataset()
 
-ViT_dataset = DatasetPreparer()
+ViT_dataset = DatasetPreparer(model_type="vit")
 _, _, vit_test_loader = ViT_dataset.prepare_dataset()
 
 # Extract features for both models
