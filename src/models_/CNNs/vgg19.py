0b212675f0a0c7b9fc37d5a92dbd202422b7fd72
HJacksons <jacksonherberts@gmail.com>
2024-02-13 12:21:17 +0100

    train vgg19

--- src/models_/CNNs/vgg19.py
+++ src/models_/CNNs/vgg19.py
@@ -8,6 +8,8 @@ class VGG19(nn.Module):
         super(VGG19, self).__init__()
 
         self.model = models.vgg19(pretrained=True)
+        for param in self.model.parameters():
+            param.requires_grad = False
         self.model.classifier[6] = nn.Linear(
             4096, 3
         )  # Fine turning the model for 3 classes
@@ -19,3 +21,8 @@ class VGG19(nn.Module):
 # Print model summary
 model = VGG19()
 summary(model, input_size=(3, 256, 256))
+# print trainable and non tainable prameters
+total_params = sum(p.numel() for p in model.parameters())
+print(f"{total_params:,} total parameters.")
+total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"{total_trainable_params:,} training parameters.")
