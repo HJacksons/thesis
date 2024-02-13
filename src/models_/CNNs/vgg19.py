e93b961e49cbdfe852235dfa30b5d77ac214fd58
HJacksons <jacksonherberts@gmail.com>
2024-02-13 12:32:31 +0100

    train vgg19

--- src/models_/CNNs/vgg19.py
+++ src/models_/CNNs/vgg19.py
@@ -1,13 +1,14 @@
 import torch.nn as nn
 from torchsummary import summary
 from torchvision import models
+from torchvision.models.vgg import VGG19_Weights
 
 
 class VGG19(nn.Module):
     def __init__(self):
         super(VGG19, self).__init__()
 
-        self.model = models.vgg19(pretrained=True)
+        self.model = models.vgg19(weights=VGG19_Weights.DEFAULT)
         for param in self.model.parameters():
             param.requires_grad = False
         self.model.classifier[6] = nn.Linear(
@@ -19,10 +20,10 @@ class VGG19(nn.Module):
 
 
 # Print model summary
-model = VGG19()
-summary(model, input_size=(3, 256, 256))
-# print trainable and non tainable prameters
-total_params = sum(p.numel() for p in model.parameters())
-print(f"{total_params:,} total parameters.")
-total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(f"{total_trainable_params:,} training parameters.")
+# model = VGG19()
+# summary(model, input_size=(3, 256, 256))
+# # print trainable and non tainable prameters
+# total_params = sum(p.numel() for p in model.parameters())
+# print(f"{total_params:,} total parameters.")
+# total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+# print(f"{total_trainable_params:,} training parameters.")
