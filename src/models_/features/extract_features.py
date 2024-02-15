07b2eadcf19be907df27d694b07a7cffc551e0a6
HJacksons <jacksonherberts@gmail.com>
2024-02-15 16:57:23 +0100

    train extract

--- src/models_/features/extract_features.py
+++ src/models_/features/extract_features.py
@@ -33,7 +33,7 @@ def extract_features_directly(
 
     # Navigate to the layer
     target_layer = model
-    for attr in layer_path:
+    for attr in layer_path.split("."):
         target_layer = getattr(target_layer, attr)
 
     # Define a hook to capture the output of the specified layer
