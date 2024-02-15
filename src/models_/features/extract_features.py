7e48a6608355eefdbd0c9fe8d71672e2067a5165
HJacksons <jacksonherberts@gmail.com>
2024-02-15 17:52:29 +0100

    train extract

--- src/models_/features/extract_features.py
+++ src/models_/features/extract_features.py
@@ -31,7 +31,7 @@ class ModelFeatureExtractor:
         global features_vgg
         features_vgg = None
         # Assuming 'features' is the last conv layer in VGG for feature extraction
-        layer = self.model.features
+        layer = self.model.model.features
         layer.register_forward_hook(self.vgg_hook)
 
     def inception_hook(self, module, inputs, output):
